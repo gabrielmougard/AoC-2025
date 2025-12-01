7d06d9b2052e433d4e816b8aebd703019945b935
Gabriel Mougard <gabriel.mougard@gmail.com>
2025-12-01 23:50:18 +0100

    part 2 of day 1

--- 01-secret-entrance/main.zig
+++ 01-secret-entrance/main.zig
@@ -3,31 +3,49 @@ const math = std.math;
 
 const input_data = @embedFile("input.txt");
 
-// Result: 969
-// Time: 41625ns (41μs)
+fn countZerosLeft(state: i32, moves: i32) i32 {
+    if (state == 0) {
+        return @divFloor(moves, 100);
+    } else if (moves >= state) {
+        return @divFloor(moves - state, 100) + 1;
+    } else {
+        return 0;
+    }
+}
+
+fn countZerosRight(state: i32, moves: i32) i32 {
+    return @divFloor(state + moves, 100);
+}
+
 pub fn main() !void {
     var timer = try std.time.Timer.start();
     var out = std.fs.File.stdout().writerStreaming(&.{});
 
     var line_iter = std.mem.splitScalar(u8, input_data, '\n');
     var state: i32 = 50;
-    var new_state: i32 = 0;
-    var direction: ?u8 = null;
-    var moves: i32 = 0;
-    var res: i32 = 0;
+    var endpoints_at_zero: i32 = 0;
+    var total_zero_clicks: i32 = 0;
+
     while (line_iter.next()) |line| {
         if (line.len == 0) continue;
 
-        direction = line[0];
-        moves = std.fmt.parseInt(i32, line[1..], 10) catch continue;
-        new_state = switch (direction.?) {
+        const direction = line[0];
+        const moves = std.fmt.parseInt(i32, line[1..], 10) catch continue;
+        const zeros = switch (direction) {
+            'L' => countZerosLeft(state, moves),
+            'R' => countZerosRight(state, moves),
+            else => 0,
+        };
+
+        total_zero_clicks += zeros;
+        const new_state = switch (direction) {
             'L' => try math.mod(i32, state - moves, 100),
             'R' => try math.mod(i32, state + moves, 100),
             else => state,
         };
 
         if (new_state == 0) {
-            res += 1;
+            endpoints_at_zero += 1;
         }
 
         state = new_state;
@@ -35,6 +53,7 @@ pub fn main() !void {
 
     const elapsed = timer.read();
 
-    try out.interface.print("Result: {d}\n", .{res});
+    try out.interface.print("Password (endpoint method): {d}\n", .{endpoints_at_zero});
+    try out.interface.print("Password (click method): {d}\n", .{total_zero_clicks});
     try out.interface.print("Time: {d}ns ({d}μs)\n", .{ elapsed, elapsed / 1000 });
 }
